b2768ba371a9c08e5acfaeeb90a58ec9ed23bdd5
Christopher Torres <torrinkrisl@gmail.com>
2019-07-16 10:35:11 -0600

    Original file structure

---
+++ routes/api-routes.js
@@ -0,0 +1,8 @@
+var db = require("../models");
+var passport = require("../config/passport");
+var Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+module.exports = function(app){
+
+}
